7510740782e9e441ecc6cc05adf6cab29af70dc4
Кузьмин Тимур Юрьевич <T.Kuzmin@npr.procervic.ru>
2019-10-17 17:45:47 +0700

    Customize admin views

--- LocalLibrary/catalog/admin.py
+++ LocalLibrary/catalog/admin.py
@@ -2,9 +2,38 @@ from django.contrib import admin
 from .models import Author, Genre, Book, Bookinstance, Language
 
 
+# Define the admin class
+class BooksInstanceInline(admin.TabularInline):
+    model = Bookinstance
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+@admin.register(Bookinstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
 # Register your models here.
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(Bookinstance)
+# admin.site.register(Book)
+# admin.site.register(Bookinstance)
 admin.site.register(Language)
